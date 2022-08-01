4ad70c99f6a5e32175ee4b668185c33cb7f56d89
Mateusz <ratusznikmateusz@gmail.com>
2022-08-01 14:39:12 +0200

    zad. 1 zrobione

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const body = document.querySelector("body");
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  };
+
+let interval;
+
+btnStart.addEventListener("click", () => { interval = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+    btnStart.disabled = true;
+    btnStop.disabled = false;
+}, 1000);
+})
+//const color = getRandomHexColor();
+
+btnStop.addEventListener("click", () => {
+  clearInterval(interval);
+  btnStart.disabled = false;
+  btnStop.disabled = true;
+});
